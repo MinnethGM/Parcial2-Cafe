ce71bc8e3ef1b9bea424bc0a32cdd6f89d2351c0
Alumno <ulsa@iMacD112.local>
2018-10-16 19:06:36 -0700

    Parcial

---
+++ ViewControllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Nuevocafe
+//
+//  Created by Alumno on 15/10/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var _Username: UITextField!
+    
+    @IBOutlet weak var _Password: UITextField!
+    
+    
+    @IBOutlet weak var _Button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
